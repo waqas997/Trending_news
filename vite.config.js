842b8e1c1f9252d454a4934c71a7001e8e8304e2
waqas997 <139737784+waqas997@users.noreply.github.com>
2026-09-21 01:42:41 +0500

    Add local proxy for API development

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://trending-news-one.vercel.app',
+        changeOrigin: true,
+      }
+    }
+  }
 })
